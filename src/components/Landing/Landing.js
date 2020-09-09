dee2ccbed9a20e12c4fa5381f890452578facfe4
martin-wachira <martinnwachira@gmail.com>
2020-09-09 21:43:05 +0300

    added adminlte template

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import styles from './Landing.module.scss';
+import AdminLTE, { Sidebar, Content, Row, Col, Box, Button } from 'adminlte-2-react';
 
 const Landing = () => (
   <div className={styles.Landing} data-testid="Landing">
     <h2>
     Create introductory info here plus account login/registration links
     </h2>
+    <Row>
+        <Col xs={6}>
+          <Box title="My first box" type="primary" closable collapsable footer={<Button type="danger" text="Danger Button" />}>
+            Hello World
+          </Box>
+        </Col>
+        <Col xs={6}>
+          <Box title="Another box">
+            Content goes here
+          </Box>
+        </Col>
+      </Row>
   </div>
 );
 
