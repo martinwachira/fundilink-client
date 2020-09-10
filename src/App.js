788290e71b07eb6cb641b99fa2376e4c5d0fc1aa
martin-wachira <martinnwachira@gmail.com>
2020-09-10 22:16:09 +0300

    header fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import Signin from './components/Signin/Signin';
 import Dashboard from './components/Dashboard/Dashboard';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <Router>
     <Switch>
-      <Route exact path="/" component={Landing} />
+      <Route exact path="/" component={Header} />
       <Route exact path="/login" component={Signin} />
       <Route exact path="/dashboard" component={Dashboard} />
       <Route exact={true} path="*" component={Landing} />
