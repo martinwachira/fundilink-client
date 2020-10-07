ca79af34afef82e252cab401e93add0100bc5db7
martin-wachira <martinnwachira@gmail.com>
2020-10-07 20:23:08 +0300

    Landing header

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -5,15 +5,25 @@ import EmployerSignup from '../EmployerSignup/EmployerSignup';
 
 const Landing = () => (
  <div classname="{styles.Landing}" data-testid="Landing">
+  <div className={styles.heads}>
+  <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand">FundiLink</a>
+    <form className="form-inline">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </nav>
+  </div>
    <div className="container">
-   <h3>Select user type here</h3>
-    <div class="form-group">
-      <label for="role">User Role</label>
-      <select class="custom-select" name="" id="">
-        <option selected>Select role</option>
-        <option value="">Employer</option>
-        <option value="">Service Provider</option>        
-      </select>
+
+  <h3>Select user type here</h3>
+  <div className="form-group">
+    <label htmlFor="role">User Role</label>
+    <select className="custom-select" name id>
+      <option selected>Select role</option>
+      <option value>Employer</option>
+      <option value>Service Provider</option>
+    </select>    
     </div>     
    </div>
 {/* <EmployerSignup /> */}
